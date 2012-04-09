0ee55713d2105d2a653fde2900fa79bf3f5ae6e6
Timothy Andrew <mail@timothyandrew.net>
2012-04-09 23:46:28 +0530

    fix precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Hangout::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.precompile += %w[active_admin.css active_admin.js]
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
